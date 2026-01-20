518544cdd962a04682d986f97992f207a118065c
Zaid Khan <zaidcodingtopcoding@gmail.com>
2026-01-20 17:06:56 +0500

    changed the frontend link

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const app = express();
 // Middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://ecommerce-zubrand.vercel.app",
     credentials: true,
   })
 );
